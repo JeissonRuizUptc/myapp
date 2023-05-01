b224f4f4dc99530127760a7dfa992351e85c9831
JeissonRuizUptc <90711266+JeissonRuizUptc@users.noreply.github.com>
2023-05-01 15:45:50 -0500

    [LOGIN STABLE]

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -9,21 +9,24 @@ const FormLogin = () => {
             <h2>Iniciar Sesion</h2>
             <div className="formLogin_form">
 
-                <label htmlFor="">usuario</label>
+                <label htmlFor="">Usuario</label>
                 <div className="inputName">
                     <input type="text" className="form-control" />
                 </div>
-                
+
                 <label htmlFor="">Contraseña</label>
                 <div className="inputPassword">
                     <input type="password" className="form-control" />
                 </div>
-                
+
 
                 <div className="divButton">
-                    <button className="btn btn-primary">Entrar</button>
+                    <a href="https://www.google.com/webhp?hl=es&ictx=2&sa=X&ved=0ahUKEwiUzcan_dT-AhWaTjABHWFeBEoQPQgJ">
+                        <button className="btn btn-secondary">Entrar</button>
+                    </a>
+                    
                 </div>
-                
+
                 <a href="">Registrarse</a>
                 <a href="">Olvide mi contraseña</a>
             </div>
